e1fea1f3f454cdefa5d5dadbc024d871c1c87389
Angel Ramirez <angelgabriel.16ra@gmail.com>
2025-04-15 12:16:25 -0600

    Creating tables related to users part 2

--- src/migrations/1743373783230-migrations.ts
+++ src/migrations/1743373783230-migrations.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner, Table,TableColumn,TableForeignKey } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table,TableColumn,TableForeignKey, TableIndex } from 'typeorm';
 
 export class Migrations1743373783230 implements MigrationInterface {
 
@@ -70,6 +70,14 @@ export class Migrations1743373783230 implements MigrationInterface {
               ]
           }),
         );
+
+        await queryRunner.createIndex(
+          'users',
+          new TableIndex({
+              name:'IDX_USERS_ID',
+              columnNames:['id']
+          }),
+        )
         await queryRunner.createTable(
           new Table({
               name:'user_preference',
@@ -117,6 +125,14 @@ export class Migrations1743373783230 implements MigrationInterface {
 
               ]
           }),)
+
+        await queryRunner.createIndex(
+          'user_preference',
+          new TableIndex({
+              name:'IDX_PREFERENCE_ID',
+              columnNames:['id']
+          }),
+        )
         await queryRunner.createTable(
           new Table({
               name:'user_sessions',
@@ -147,6 +163,15 @@ export class Migrations1743373783230 implements MigrationInterface {
                   },
               ]
           }),)
+
+        await queryRunner.createIndex(
+          'user_sessions',
+          new TableIndex({
+              name:'IDX_USER_SESSIONS',
+              columnNames:['id']
+          }),
+        )
+
         await queryRunner.createTable(
           new Table({
               name:'suggestions',
@@ -214,6 +239,14 @@ export class Migrations1743373783230 implements MigrationInterface {
                   },
               ]
           }),)
+
+        await queryRunner.createIndex(
+          'suggestions',
+          new TableIndex({
+              name:'IDX_SUGGESTIONS_ID',
+              columnNames:['id']
+          }),
+        )
         await queryRunner.createTable(
           new Table({
               name:'notifications',
@@ -264,8 +297,19 @@ export class Migrations1743373783230 implements MigrationInterface {
                       isNullable: true,
                   },
               ]
-          }),)
+          }),
+        )
 
+        await queryRunner.createIndex(
+          'notifications',
+          new TableIndex({
+              name:'IDX_NOTIFICATIONS_ID',
+              columnNames:['id']
+          }),
+        )
+
+        {/*
+        // Add Column users-user_preference
         await queryRunner.addColumn(
           'users',
           new TableColumn({
@@ -283,8 +327,120 @@ export class Migrations1743373783230 implements MigrationInterface {
               onDelete:'CASCADE',
               onUpdate:'CASCADE'
           }),)
+          */}
+      {/*
+        // Add Column users-user_sessions
+        await queryRunner.addColumn(
+          'users',
+          new TableColumn({
+              name:'user_sessions_id',
+              type:'varchar',
+              isNullable:false,
+          })
+        )
+        await queryRunner.createForeignKey(
+          'users',
+          new TableForeignKey(
+            {
+                columnNames:['user_sessions_id'],
+                referencedColumnNames:['id'],
+                referencedTableName:'user_sessions',
+                onDelete:'CASCADE',
+                onUpdate:'CASCADE'
+            }
+          )
+        )
+      */}
+        // Add Column user_preference-users
+
+        await queryRunner.addColumn(
+          'user_preference',
+          new TableColumn({
+              name:'user_id',
+              type:'varchar',
+              isNullable:false,
+          })
+        )
+        await queryRunner.createForeignKey(
+          'user_preference',
+          new TableForeignKey({
+              columnNames:['user_id'],
+              referencedColumnNames:['id'],
+              referencedTableName:'users',
+              onDelete:'CASCADE',
+              onUpdate:'CASCADE'
+          }),)
+
+
+        // Add Column user_sessions-users
+
+        await queryRunner.addColumn(
+          'user_sessions',
+          new TableColumn({
+              name:'user_id',
+              type:'varchar',
+              isNullable:false,
+          })
+        )
+        await queryRunner.createForeignKey(
+          'user_sessions',
+          new TableForeignKey(
+            {
+                columnNames:['user_id'],
+                referencedColumnNames:['id'],
+                referencedTableName:'users',
+                onDelete:'CASCADE',
+                onUpdate:'CASCADE'
+            }
+          )
+        )
+      // Add Column suggestions-users
+        await queryRunner.addColumn(
+          'suggestions',
+          new TableColumn({
+            name:'user_id',
+            type:'varchar',
+            isNullable:false,
+          })
+        )
+        await queryRunner.createForeignKey(
+          'suggestions',
+          new TableForeignKey(
+            {
+              columnNames:['user_id'],
+              referencedColumnNames:['id'],
+              referencedTableName:'users',
+              onDelete:'CASCADE',
+              onUpdate:'CASCADE'
+            }
+          )
+        )
+      // Add Column notifications-users
+      await queryRunner.addColumn(
+        'notifications',
+        new TableColumn({
+          name:'user_id',
+          type:'varchar',
+          isNullable:false,
+        })
+      )
+      await queryRunner.createForeignKey(
+        'notifications',
+        new TableForeignKey(
+          {
+            columnNames:['user_id'],
+            referencedColumnNames:['id'],
+            referencedTableName:'users',
+            onDelete:'CASCADE',
+            onUpdate:'CASCADE'
+          }
+        )
+      )
+
+
     }
     public async down(queryRunner: QueryRunner): Promise<void> {
+      {/*
         const table = await queryRunner.getTable('users')
         // @ts-ignore
         const foreignKey=table.foreignKeys.find(
@@ -294,8 +450,78 @@ export class Migrations1743373783230 implements MigrationInterface {
         await queryRunner.dropForeignKey('users',foreignKey)
         await queryRunner.dropColumn('users','user_preference_id')
         await queryRunner.dropTable('users')
-        await queryRunner.dropIndex('user_preference','IDX_USER_PREFERENCE_NAME')
+        await queryRunner.dropIndex('user_preference','IDX_PREFERENCE_ID')
         await queryRunner.dropTable('user_preference')
+      */}
+      // Add Column users-user_sessions
+      {/* const tableUsers = await queryRunner.getTable('users')
+        // @ts-ignore
+        const ForeignKeyUsers=tableUsers.foreignKeys.find(
+          (fk)=>fk.columnNames.indexOf('user_sessions_id')!==-1,
+        )
+        // @ts-ignore
+        await queryRunner.dropForeignKey('users',ForeignKeyUsers)
+        await queryRunner.dropColumn('users','user_sessions_id')
+        await queryRunner.dropTable('users')
+        await queryRunner.dropIndex('user_sessions','IDX_USER_SESSIONS')
+        await queryRunner.dropTable('user_sessions')
+          */}
+
+        const tablePreference = await queryRunner.getTable('user_preference')
+        // @ts-ignore
+        const foreignKeyPreference=tablePreference.foreignKeys.find(
+          (fk)=>fk.columnNames.indexOf('user_id')!==-1,
+        )
+        // @ts-ignore
+        await queryRunner.dropForeignKey('user_preference',foreignKeyPreference)
+        await queryRunner.dropColumn('user_preference','user_id')
+        await queryRunner.dropTable('user_preference')
+        await queryRunner.dropIndex('users','IDX_USERS_ID')
+        await queryRunner.dropTable('users')
+
+        // Add Column user_sessions-users
+
+        const tableSessions = await queryRunner.getTable('user_sessions')
+        // @ts-ignore
+        const ForeignKeySessions=tableSessions.foreignKeys.find(
+          (fk)=>fk.columnNames.indexOf('user_id')!==-1,
+        )
+        // @ts-ignore
+        await queryRunner.dropForeignKey('user_sessions',ForeignKeySessions)
+        await queryRunner.dropColumn('user_sessions','user_id')
+        await queryRunner.dropTable('user_sessions')
+        await queryRunner.dropIndex('users','IDX_USERS_ID')
+        await queryRunner.dropTable('users')
+
+      // Add Column suggestions-users
+
+      const tableSuggestions = await queryRunner.getTable('suggestions')
+      // @ts-ignore
+      const ForeignKeySuggestions=tableSuggestions.foreignKeys.find(
+        (fk)=>fk.columnNames.indexOf('user_id')!==-1,
+      )
+      // @ts-ignore
+      await queryRunner.dropForeignKey('suggestions',ForeignKeySuggestions)
+      await queryRunner.dropColumn('suggestions','user_id')
+      await queryRunner.dropTable('suggestions')
+      await queryRunner.dropIndex('users','IDX_USERS_ID')
+      await queryRunner.dropTable('users')
+
+
+      // Add Column notifications-users
+
+      const tableNotifications = await queryRunner.getTable('notifications')
+      // @ts-ignore
+      const ForeignKeyNotifications=tableNotifications.foreignKeys.find(
+        (fk)=>fk.columnNames.indexOf('user_id')!==-1,
+      )
+      // @ts-ignore
+      await queryRunner.dropForeignKey('notifications',ForeignKeyNotifications)
+      await queryRunner.dropColumn('notifications','user_id')
+      await queryRunner.dropTable('notifications')
+      await queryRunner.dropIndex('users','IDX_USERS_ID')
+      await queryRunner.dropTable('users')
+
     }
 }
 

596f9c231868f49aafa3198ea3088869959e8205
Angel Ramirez <angelgabriel.16ra@gmail.com>
2025-05-20 22:20:46 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     transform:true,
   }));
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('APP_PORT') || 3000;
+  const port = process.env.PORT || configService.get<number>('APP_PORT') || 3000;
   await app.listen(port);
 }
 bootstrap();
